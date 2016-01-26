b31738d53c53745ba9b631baad8dfa32263019c3
Roberto Viquez <robertoviquez@gmail.com>
2016-01-26 17:07:05 -0600

    Finished CH05

--- Ex_Files_Nodejs_EssT/Exercise Files/Ch05/05_04/start/directory.js
+++ Ex_Files_Nodejs_EssT/Exercise Files/Ch05/05_04/start/directory.js
@@ -2,5 +2,14 @@
  * Created by rviquez on 1/25/2016.
  */
 var fs = require("fs");
-
-fs.mkdir("")
+if (fs.existsSync("lib")) {
+    console.log("Directory already exists");
+}else{
+    fs.mkdir("lib", function (err) {
+        if (err) {
+            console.log(err);
+        }else{
+            console.log("Directory Created.");
+        }
+    });
+}
